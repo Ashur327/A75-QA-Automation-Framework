4d2c1910f818109f2d94fe8ddf2f56da4b5777d4
Ashur327 <ashur.yonan@testpro.io>
2025-10-20 15:14:52 -0700

    setup for jenkins

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -17,9 +17,9 @@ public class LoginTests extends BaseTest {
                           @Optional("") String cloudAccessKey) throws Exception {
 
         // BaseTest's @BeforeMethod already sets up driver and credentials
-        // Just retrieve it for this test
-        WebDriver driver = getDriver();
+        super.setup(email, password, browser, cloudUserName, cloudAccessKey);
 
+        WebDriver driver = getDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(baseUrl);
 
@@ -28,15 +28,15 @@ public class LoginTests extends BaseTest {
     }
 
     @Test(groups = {"smoke"})
-    @Parameters({"email", "password", "baseUrl"})
-    public void validLoginTest(String email, String password, String baseUrl) {
+    @Parameters({"baseUrl"})
+    public void validLoginTest(String baseUrl) {
+        // use BaseTest fields for credentials
         loginPage.login(email, password);
         Assert.assertEquals(getDriver().getCurrentUrl(), baseUrl + "#!/home");
     }
 
     @AfterMethod
     public void teardownTest() {
-        // driver cleanup handled by BaseTest
         super.tearDown();
     }
 }
